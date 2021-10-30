52a9e00622ea0faf02f0c54cc29e93f04a8cf7c1
Igrium <thesam54123@gmail.com>
2021-10-30 17:08:18 -0400

    transitioned away from example mod

---
+++ src/main/java/org/scaffoldeditor/worldexport/WorldExport.java
@@ -0,0 +1,17 @@
+package org.scaffoldeditor.worldexport;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WorldExport implements ClientModInitializer {
+
+    public static final Logger LOGGER = LogManager.getLogger("worldexport");
+
+    @Override
+    public void onInitializeClient() {
+        LOGGER.info("Initializing world export mod.");
+    }
+    
+}
