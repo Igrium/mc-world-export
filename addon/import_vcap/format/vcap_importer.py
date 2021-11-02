3fdc73eacd2211ba527c4966a5e76b13e0eff21f
Igrium <thesam54123@gmail.com>
2021-11-02 15:15:27 -0400

    complete import draft

--- addon/import_vcap/format/vcap_importer.py
+++ addon/import_vcap/format/vcap_importer.py
@@ -11,7 +11,7 @@ from . import import_obj
 from .world import VCAPWorld
 
 from .. import amulet_nbt
-from ..amulet_nbt import TAG_Compound, TAG_List, TAG_Byte_Array
+from ..amulet_nbt import TAG_Compound, TAG_List, TAG_Byte_Array, TAG_String
 
 class VCAPContext:
     archive: ZipFile
@@ -69,11 +69,10 @@ def load(file: str, collection: Collection, context: Context):
     archive = ZipFile(file, 'r')
     world_dat = archive.open('world.dat')
     
-    vcap_context = VCAPContext(archive, collection, context)
-    loadMeshes(archive, vcap_context)
-    print(vcap_context.models)
-    readWorld(world_dat)
-    world_dat.close
+    vcontext = VCAPContext(archive, collection, context)
+    loadMeshes(archive, vcontext)
+    readWorld(world_dat, vcontext)
+    world_dat.close()
 
 def loadMeshes(archive: ZipFile, context: VCAPContext):
     for file in archive.filelist:
@@ -81,18 +80,42 @@ def loadMeshes(archive: ZipFile, context: VCAPContext):
             model_id = os.path.splitext(os.path.basename(file.filename))[0]
             context.get_mesh(model_id)
 
-def readWorld(world_dat: IO[bytes]):
+def readWorld(world_dat: IO[bytes], vcontext: VCAPContext):
     nbt: amulet_nbt.NBTFile = amulet_nbt.load(world_dat.read(), compressed=False)
     world = VCAPWorld(nbt.value)
-
-
+    print("Loading world...")
 
     frame = world.get_frame(0)
+    sections: TAG_List = frame['sections']
+    for i in range(0, len(sections)):
+        print(f'Parsing section {i + 1} / {len(sections)}')
+        readSection(sections[i], vcontext)
 
-def readSection(section: TAG_Compound):
+def readSection(section: TAG_Compound, vcontext: VCAPContext):
     palette: TAG_List = section['palette']
     offset: tuple[int, int, int] = (section['x'].value, section['y'].value, section['z'].value)
     blocks: TAG_Byte_Array = section['blocks']
     bblocks = blocks.value
 
-    b = bblocks.item(1)
+    for y in range(0, 16):
+        for z in range(0, 16):
+            for x in range(0, 16):
+                index = bblocks.item((y * 16 + z) * 16 + x)
+                model_id: TAG_String = palette[index]
+
+                place(model_id.value, pos=(offset[0] * 16 + x, offset[1] * 16 + y, offset[2] * 16 + z), vcontext=vcontext)
+
+def place(model_id: str, pos: tuple[float, float, float], vcontext: VCAPContext):
+    # if not (model_id in vcontext.models):
+    #     raise RuntimeError(f'Model {model_id} does not have a mesh!')
+    mesh = vcontext.models[model_id]
+
+    if (len(mesh.vertices) == 0): return
+
+    obj: Object = bpy.data.objects.new("block"+str(pos), mesh)
+    vcontext.collection.objects.link(obj)
+
+    obj.location = pos
+    return obj
+
+    
