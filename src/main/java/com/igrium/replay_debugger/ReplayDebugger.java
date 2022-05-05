fa905629486723765b0368c569c7a1aec7e239b7
Igrium <thesam54123@gmail.com>
2022-05-05 13:51:34 -0700

    started work on replay debugger

--- src/main/java/com/igrium/replay_debugger/ReplayDebugger.java
+++ src/main/java/com/igrium/replay_debugger/ReplayDebugger.java
@@ -1,9 +1,39 @@
 package com.igrium.replay_debugger;
 
-import javax.swing.JFrame;
+import java.awt.AWTException;
+import java.awt.HeadlessException;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import org.apache.logging.log4j.LogManager;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
 public class ReplayDebugger {
-    public void launch() {
-        JFrame frame = new JFrame();
+    private JFrame frame;
+
+    public void launch() throws HeadlessException {
+        System.setProperty("java.awt.headless", "false"); // This could easily fuck things up
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+                | UnsupportedLookAndFeelException e) {
+            LogManager.getLogger().error("Error setting debugger look and feel.", e);
+        }
+        frame = new JFrame();
+
+        JButton b = new JButton("click");
+        b.setBounds(130, 100, 100, 40);
+        frame.add(b);
+
+        frame.setSize(400, 500);
+        frame.setLayout(null);
+        frame.setVisible(true);
     }
 }
---
+++ src/main/java/com/igrium/replay_debugger/mixins/TitleScreenMixin.java
@@ -0,0 +1,39 @@
+package com.igrium.replay_debugger.mixins;
+
+import java.awt.HeadlessException;
+
+import com.igrium.replay_debugger.ReplayDebugger;
+
+import org.apache.logging.log4j.LogManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
+
+@Mixin(TitleScreen.class)
+public class TitleScreenMixin extends Screen {
+
+    protected TitleScreenMixin(Text title) {
+        super(title);
+    }
+
+    @Inject(method = "init()V", at = @At("RETURN"))
+    protected void init(CallbackInfo ci) {
+        addDrawableChild(new ButtonWidget(width - 98, 0, 98, 20, new LiteralText("Debug Replays"), (button) -> {
+            try {
+                ReplayDebugger instance = new ReplayDebugger();
+                instance.launch();
+            } catch (HeadlessException e) {
+                LogManager.getLogger(ReplayDebugger.class).error("Unable to launch debugger in headless environment.");
+            }
+            
+		}));
+    }
+    
+}
