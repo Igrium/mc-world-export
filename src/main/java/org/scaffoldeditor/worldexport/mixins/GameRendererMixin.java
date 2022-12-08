de65e063beb8d07ad9ce27829b937fc1ace977d8
Igrium <thesam54123@gmail.com>
2022-12-08 00:55:47 -0500

    implemented camera anim roll

--- src/main/java/org/scaffoldeditor/worldexport/mixins/GameRendererMixin.java
+++ src/main/java/org/scaffoldeditor/worldexport/mixins/GameRendererMixin.java
@@ -1,17 +1,21 @@
 package org.scaffoldeditor.worldexport.mixins;
 
 import org.scaffoldeditor.worldexport.replaymod.util.FovProvider;
+import org.scaffoldeditor.worldexport.replaymod.util.RollProvider;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.math.Vec3f;
 
 @Mixin(GameRenderer.class)
 public class GameRendererMixin {    
@@ -24,6 +28,14 @@ public class GameRendererMixin {
         Entity entity = this.client.getCameraEntity() == null ? this.client.player : this.client.getCameraEntity();
         if (!camera.isThirdPerson() && entity instanceof FovProvider) {
             ci.setReturnValue(((FovProvider) entity).getFov());
+        }
+    }
+
+    @Inject(method = "renderWorld", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/Camera;getPitch()F"))
+    void applyRoll(float tickDelta, long limitTime, MatrixStack matrices, CallbackInfo ci) {
+        Entity entity = this.client.getCameraEntity() == null ? this.client.player : this.client.getCameraEntity();
+        if (entity instanceof RollProvider) {
+            matrices.multiply(Vec3f.NEGATIVE_Z.getDegreesQuaternion(((RollProvider) entity).getRoll()));
         }
     }
 }
