8a1e83d4a1c51f5e374de461a6ce0cdfceea819b
Sam54123 <thesam54123@gmail.com>
2023-04-04 14:53:19 -0700

    got zoom and pan working in the bounds editor

--- src/main/java/org/scaffoldeditor/worldexport/gui/bounds_editor/GuiBoundsOverview.java
+++ src/main/java/org/scaffoldeditor/worldexport/gui/bounds_editor/GuiBoundsOverview.java
@@ -3,9 +3,15 @@ package org.scaffoldeditor.worldexport.gui.bounds_editor;
 import java.io.Closeable;
 import java.util.Objects;
 
+import org.joml.Vector2f;
+import org.joml.Vector2fc;
+import org.lwjgl.glfw.GLFW;
+
 import com.replaymod.lib.de.johni0702.minecraft.gui.GuiRenderer;
 import com.replaymod.lib.de.johni0702.minecraft.gui.RenderInfo;
 import com.replaymod.lib.de.johni0702.minecraft.gui.element.AbstractGuiElement;
+import com.replaymod.lib.de.johni0702.minecraft.gui.function.Draggable;
+import com.replaymod.lib.de.johni0702.minecraft.gui.function.Scrollable;
 import com.replaymod.lib.de.johni0702.minecraft.gui.utils.lwjgl.Dimension;
 import com.replaymod.lib.de.johni0702.minecraft.gui.utils.lwjgl.ReadableDimension;
 import com.replaymod.lib.de.johni0702.minecraft.gui.utils.lwjgl.ReadablePoint;
@@ -15,16 +21,15 @@ import net.minecraft.client.texture.NativeImage;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Util;
-import net.minecraft.util.math.Vec2f;
 import net.minecraft.world.World;
 
-public class GuiBoundsOverview extends AbstractGuiElement<GuiBoundsOverview> implements Closeable {
+public class GuiBoundsOverview extends AbstractGuiElement<GuiBoundsOverview> implements Closeable, Draggable, Scrollable {
 
     private final OverviewData overviewData;
     private final Identifier texID;
     private final World world;
 
-    private Vec2f panOffset = Vec2f.ZERO;
+    private Vector2f panOffset = new Vector2f();
     private double zoomAmount = 0;
 
     public GuiBoundsOverview(World world, OverviewData overviewData) {
@@ -45,12 +50,12 @@ public class GuiBoundsOverview extends AbstractGuiElement<GuiBoundsOverview> imp
         this.zoomAmount = zoomAmount;
     }
 
-    public Vec2f getPanOffset() {
+    public Vector2f getPanOffset() {
         return panOffset;
     }
 
-    public void setPanOffset(Vec2f panOffset) {
-        this.panOffset = panOffset;
+    public void setPanOffset(Vector2fc panOffset) {
+        this.panOffset.set(panOffset);
     }
 
     public void updateTexture() {
@@ -115,6 +120,50 @@ public class GuiBoundsOverview extends AbstractGuiElement<GuiBoundsOverview> imp
     @Override
     public void close() {
         overviewData.close();
+    }
+
+    @Override
+    public boolean mouseClick(ReadablePoint position, int button) {
+        // return (button == GLFW.GLFW_MOUSE_BUTTON_1);
+        return false;
+    }
+
+    @Override
+    public boolean scroll(ReadablePoint mousePosition, int dWheel) {
+        setZoomAmount(zoomAmount + (dWheel > 0 ? 1 : -1));
+        return true;
+    }
+
+    private ReadablePoint lastDragPosition;
+
+    @Override
+    public boolean mouseDrag(ReadablePoint position, int button, long timeSinseLastCall) {
+        if (button != GLFW.GLFW_MOUSE_BUTTON_1) return false;
+        
+        if (lastDragPosition == null) {
+            lastDragPosition = position;
+            return true;
+        }
+
+        float zoomMultiplier = (float) getZoomMultiplier();
+        float deltaX = (position.getX() - lastDragPosition.getX()) / zoomMultiplier;
+        float deltaY = (position.getY() - lastDragPosition.getY()) / zoomMultiplier;
+        
+        this.panOffset.x += deltaX;
+        this.panOffset.y += deltaY;
+
+        lastDragPosition = position;
+        return true;
+    }
+
+    @Override
+    public boolean mouseRelease(ReadablePoint position, int button) {
+        if (lastDragPosition != null) {
+            lastDragPosition = null;
+            return true;
+        } else {
+            return false;
+        }
     }
     
 }
