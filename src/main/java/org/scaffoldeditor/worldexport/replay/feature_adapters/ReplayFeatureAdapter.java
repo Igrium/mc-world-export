e8b1c281a24810817de7179d6b12e611729b0310
Igrium <thesam54123@gmail.com>
2025-06-27 22:25:33 -0700

    reset codebase for rewrite

--- src/main/java/org/scaffoldeditor/worldexport/replay/feature_adapters/ReplayFeatureAdapter.java
+++
@@ -1,9 +0,0 @@
-package org.scaffoldeditor.worldexport.replay.feature_adapters;
-
-import org.scaffoldeditor.worldexport.mat.MaterialConsumer;
-import org.scaffoldeditor.worldexport.replay.models.ReplayModel.Pose;
-
-public interface ReplayFeatureAdapter<T> {
-    void writePose(Pose<T> pose, float tickDelta);
-    void generateMaterials(MaterialConsumer consumer);
-}
