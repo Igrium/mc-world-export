72cdf2733236b52bd9593b1297c0a7f425113715
Igrium <thesam54123@gmail.com>
2025-07-28 22:06:27 -0700

    replay importer can now import zip files

--- worldexport_blender/replay_importer.py
+++ worldexport_blender/replay_importer.py
@@ -1,15 +1,32 @@
 import bpy
 from dataclasses import dataclass
 from bpy.types import Context, Collection
-from .replay_types import ReplayImportSettings, ReplayImportContext
 import os
+import tempfile
+import zipfile
 from . import materials
+from .replay_types import ReplayImportSettings, ReplayImportContext
 
 from . import world_importer
 from .entity import entity_loader
 
 
-def import_replay(replay_root: str, settings: ReplayImportSettings, bl_context: Context):
+def import_replay(file: str, settings: ReplayImportSettings, bl_context: Context, extract_zip: bool | None = None):
+    if extract_zip == None:
+        extract_zip = os.path.isfile(file)
+    
+    if extract_zip:
+        temp_dir = tempfile.TemporaryDirectory()
+        replay_root = temp_dir.name
+        
+        with zipfile.ZipFile(file, 'r') as zip:
+            zip.extractall(replay_root)
+            print(f'Extracted replay file to {replay_root}')
+    else:
+        temp_dir = None
+        replay_root = file
+    
+    existing_tex = set(bpy.data.images)
     existing_mats = set(bpy.data.materials)
     
     scene = bl_context.scene
@@ -27,8 +44,14 @@ def import_replay(replay_root: str, settings: ReplayImportSettings, bl_context: 
     entity_loader.import_entities(context)
     
     new_mats = set(bpy.data.materials) - existing_mats
+    new_tex = set(bpy.data.images) - existing_tex
     
     if settings.process_materials:
         materials.process_materials(new_mats)
-    
-    
+        
+    if temp_dir:
+        
+        for tex in new_tex:
+            tex.pack()
+        
+        temp_dir.cleanup()
